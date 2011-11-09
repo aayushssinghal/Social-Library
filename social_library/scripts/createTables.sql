6fa0d81a096f729feb34f556f8ca3724e01a186c
Aayush Singhal <aayushssinghal@gmail.com>
2011-11-09 14:56:43 +0530

    new folders

---
+++ social_library/scripts/createTables.sql
@@ -0,0 +1,13 @@
+create table book (
+    accno   numeric(8, 0),
+    title   varchar(30),
+    author  varchar(30),
+    publication     varchar(40),
+    edition numeric(3,0),
+    volume  numeric(3,0),
+    yyyy    numeric(4,0),
+    pages   numeric(5,0),
+    subject varchar(30),
+    ddc     varchar(30),
+    sears   varchar(30)
+);
